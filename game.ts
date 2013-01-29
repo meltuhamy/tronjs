d18b35cc42fdecb1d4492dc90a191d33df610a87
gh410 <gh410@doc.ic.ac.uk>
2013-01-29 17:07:48 +0000

    Added timing code for game loop update, set speed and canvas size to comfortable values

--- game.ts
+++ game.ts
@@ -1,39 +1,29 @@
-/* TODO: Use timer to get delta and limit FPS instead of setInterval. 
-         This allows movement code to work for any framerate by 
-         working out how fast we want to move and only updating position
-         at a certain interval, this can then be independant of graphics drawing
-         giving us a smoother experience. For more read here:
-         http://nokarma.org/2011/02/02/javascript-game-development-the-game-loop/index.html */
+var SPEED = 30.0;
+var TILESIZE = 5;
+var DIRECTIONS = {RIGHT:0,UP:1,LEFT:2,DOWN:3};
 
-/* TODO: Work out canvas size and tile size that gives us smooth gameplay 
-         and allows at least 4 players on screen at once */
-
-var FPS = 50.0;
-var TILESIZE = 10;
-
-var interval;
 var x;
 var y; 
 var direction;
 var mapArray;
 var canvas;
 var context;
+var loops = 0;
+var skipTicks = 1000 / SPEED;
+var maxFrameSkip = 10;
+var nextGameTick = (new Date).getTime();
 
-/* Called once the body of the HTML has loaded 
-   (can also call whenever you need to reset the game) */
+
+/* Called once the body of the HTML has loaded */
 function loadGame(){
-	/* Stop any previous instance of the running game (makes reset work) */
-	clearInterval(interval);
-
 	/* Initialise draw object variables */
-	interval = null;
 	canvas = document.getElementById("game");
 	context = canvas.getContext("2d");
 
 	/* Initialise player variables */
 	x = (canvas.width/TILESIZE)/2;
 	y = (canvas.height/TILESIZE)/2;
-	direction = -1;
+	direction = DIRECTIONS.RIGHT;
 
 	/* Initialise array of covered squares */
 	mapArray = new Array(canvas.width/TILESIZE);
@@ -47,47 +37,68 @@ function loadGame(){
 	/* Prepare the key listener */
 	window.addEventListener('keydown',keyDownEvent,true);
 
-	/* Draw the screen once so the player knows the game is ready.
-	   This could be replaced with show title screen or some animation */
-	drawGrid();
-	drawPlayer();
-	
+	/* Begin the game loop */
+	onEachFrame(mainLoop);
 }
 
-/* Main game loop called 'FPS'-times per second */
-function mainLoop(){
-	/* Game Logic: Move player and update grid */
+/* Reset the game state */
+function resetGame(){
+	loops = 0;
+	skipTicks = 1000 / SPEED;
+	maxFrameSkip = 10;
+	nextGameTick = (new Date).getTime();
+	x = (canvas.width/TILESIZE)/2;
+	y = (canvas.height/TILESIZE)/2;
+	direction = DIRECTIONS.RIGHT;
 
-	mapArray[x][y] = 1;
-	if(direction == 0){
-		x+=1;
-	} else if (direction == 1){
-		y-=1;
-	} else if (direction == 2){
-		x-=1;
-	} else {
-		y+=1;
+	for(var i = 0; i < mapArray.length; i++){
+		for(var j = 0; j < mapArray[i].length; j++){
+			mapArray[i][j] = 0;
+		}
 	}
+}
+
+/* Main game loop called 'SPEED'-times per second */
+function mainLoop(){
+
+	loops = 0;
+
+	/* Game Logic: Move player and update grid */
+    while ((new Date).getTime() > nextGameTick && loops < maxFrameSkip) {
+      	mapArray[x][y] = 1;
+		if(direction == DIRECTIONS.RIGHT){
+			x+=1;
+		} else if (direction == DIRECTIONS.UP){
+			y-=1;
+		} else if (direction == DIRECTIONS.LEFT){
+			x-=1;
+		} else if (direction == DIRECTIONS.DOWN){
+			y+=1;
+		}
+      nextGameTick += skipTicks;
+      loops++;
+    }
 
 	/* Check for end game conditions */
 	if(x > canvas.width/TILESIZE-1 || x < 0 || y > canvas.height/TILESIZE-1 || y < 0 || mapArray[x][y] == 1)
-		loadGame();
+		resetGame();
 
 	/* Screen drawing code */
 	context.clearRect(0, 0, canvas.width, canvas.height);
-	drawGrid();
-	drawPlayer();
+	draw();	
 }
 
-/* Populate screen with squares for each grid element that has been covered
-   already */
-function drawGrid(){
+function draw(){
+	/* Populate screen with squares for each grid element that has been covered
+       already */
 	for(var i = 0; i < mapArray.length; i++){
 		for(var j = 0; j < mapArray[i].length; j++){
-			/* Draws a grid */
-			 context.strokeStyle = "#222222";
-		 	   context.lineWidth   = 1;
-		 	   context.strokeRect(i*TILESIZE,j*TILESIZE, TILESIZE,TILESIZE); 
+			/* Draws a grid */ 
+			/*
+			context.strokeStyle = "#222222";
+		 	context.lineWidth   = 1;
+		 	context.strokeRect(i*TILESIZE,j*TILESIZE, TILESIZE,TILESIZE); 
+		 	*/
 
 		 	/* Draw squares that have been covered */
 			if(mapArray[i][j] == 1){
@@ -97,10 +108,8 @@ function drawGrid(){
 	  
 		}
 	}
-}
 
-/* Draw square for current player position */
-function drawPlayer(){
+	/* Draw square for current player position */
 	context.fillStyle = "#FF0000";
 	context.fillRect(x*TILESIZE,y*TILESIZE,TILESIZE,TILESIZE);
 }
@@ -110,28 +119,41 @@ function drawPlayer(){
 function keyDownEvent(keyEvent){
   switch (keyEvent.keyCode) {
     case 38:  /* Up arrow was pressed */
-    	if(direction != 3)
-			direction = 1;
-		if(!interval)
-			interval  = setInterval(mainLoop,1000/FPS);
+    	if(direction != DIRECTIONS.DOWN)
+			direction = DIRECTIONS.UP;
 		break;
     case 40:  /* Down arrow was pressed */
-    	if(direction != 1)
-  			direction = 3;
-		if(!interval)
-			interval  = setInterval(mainLoop,1000/FPS);
+    	if(direction != DIRECTIONS.UP)
+  			direction = DIRECTIONS.DOWN;
 		break;
     case 37:  /* Left arrow was pressed */
-    	if(direction != 0)
-  			direction = 2;
-		if(!interval)
-			interval  = setInterval(mainLoop,1000/FPS);
+    	if(direction != DIRECTIONS.RIGHT)
+  			direction = DIRECTIONS.LEFT;
 		break;
     case 39:  /* Right arrow was pressed */
-    	if(direction != 2)
-  			direction = 0;
-		if(!interval)
-			interval  = setInterval(mainLoop,1000/FPS);
+    	if(direction != DIRECTIONS.LEFT)
+  			direction = DIRECTIONS.RIGHT;
 		break;
   }
 }
+
+(function() {
+  var onEachFrame;
+  if (window.webkitRequestAnimationFrame) {
+    onEachFrame = function(cb) {
+      var _cb = function() { cb(); webkitRequestAnimationFrame(_cb); }
+      _cb();
+    };
+  } else if (window.mozRequestAnimationFrame) {
+    onEachFrame = function(cb) {
+      var _cb = function() { cb(); mozRequestAnimationFrame(_cb); }
+      _cb();
+    };
+  } else {
+    onEachFrame = function(cb) {
+      setInterval(cb, 1000 / 60);
+    }
+  }
+  
+  window.onEachFrame = onEachFrame;
+})();
